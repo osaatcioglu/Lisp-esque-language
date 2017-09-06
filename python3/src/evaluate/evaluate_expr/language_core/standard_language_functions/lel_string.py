99184e3f19ea7cd12fc8819ddd06372c047ee188
Ömer Saatcioglu <osaatcioglu@gmail.com>
2017-09-06 23:16:46 +0200

    Bug Fix: iterate the value of the Token rather than the Token itself

--- python3/src/evaluate/evaluate_expr/language_core/standard_language_functions/lel_string.py
+++ python3/src/evaluate/evaluate_expr/language_core/standard_language_functions/lel_string.py
@@ -3,7 +3,7 @@ from token import Symbols, Token
 def _split(string):
 	if string.type != Symbols.STRING:
 		raise Exception("Can't split non-strings. Got {}".format(string.type))
-	return Token(Symbols.LIST, [Token(Symbols.STRING, c) for c in string])
+	return Token(Symbols.LIST, [Token(Symbols.STRING, c) for c in string.value])
 
 lel_string = {
 	"split": _split
