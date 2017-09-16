7042b69d0d1698e657e459ea1bdd8fa95a916165
Ömer Saatcioglu <osaatcioglu@gmail.com>
2017-09-16 22:36:33 +0200

    Adding bit manipulation functions

--- python/pylel/evaluate/evaluate_expr/language_core/standard_language_functions/__init__.py
+++ python/pylel/evaluate/evaluate_expr/language_core/standard_language_functions/__init__.py
@@ -3,9 +3,12 @@ from .lel_general import LEL_GENERAL
 from .lel_list import LEL_LIST
 from .lel_math import LEL_MATH
 from .lel_string import LEL_STRING
+from .lel_bit import LEL_BIT
 
 STANDARD = LEL_GENERAL.copy()
 STANDARD.update(LEL_BOOLEAN)
 STANDARD.update(LEL_MATH)
 STANDARD.update(LEL_LIST)
 STANDARD.update(LEL_STRING)
+STANDARD.update(LEL_STRING)
+STANDARD.update(LEL_BIT)
