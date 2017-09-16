5e16a9bbe2efeb6bf4470f11377a7796b39c52d3
Ömer Saatcioglu <osaatcioglu@gmail.com>
2017-09-16 22:54:07 +0200

    Clearing the debug print Bump the version

--- python/pylel/evaluate/evaluate_expr/language_core/standard_language_functions/lel_bit.py
+++ python/pylel/evaluate/evaluate_expr/language_core/standard_language_functions/lel_bit.py
@@ -6,7 +6,6 @@ def _and(x_number, y_number):
 	return Token(Symbols.NUMBER, int(x_number.value) & int(y_number.value))
 
 def _or(x_number, y_number):
-	print("or")
 	if x_number.type != Symbols.NUMBER or y_number.type != Symbols.NUMBER:
 		raise Exception("- only operates on NUMBER type")
 	return Token(Symbols.NUMBER, int(x_number.value) | int(y_number.value))
